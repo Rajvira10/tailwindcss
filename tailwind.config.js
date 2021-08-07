f595ce5dae03bc5035a3c5ae809a22cb1b17171b
Rajvir Ahmed Shuvo <72084907+Rajvira10@users.noreply.github.com>
2021-08-07 12:44:56 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Roboto'],
+        body2: ['Noto Sans JP'],
+        body3: ['Lato']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
